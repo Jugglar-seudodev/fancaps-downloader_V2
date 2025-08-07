9b3ad83d096462c95ee74a1e6b796b600b752cd2
Jugglar <hyacver@gmail.com>
2025-08-07 03:35:10 -0600

    Update downloader.py
    test time downloader

--- scraper/downloader.py
+++ scraper/downloader.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import time
 from http.client import IncompleteRead  # Import IncompleteRead exception
 
-def _download(url, path, timeout=10, attempts=3, delay=0.01):  # Added attempts parameter
+def _download(url, path, timeout=10, attempts=3, delay=0.1):  # Added attempts parameter
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'})
     filename = os.path.join(path, url.split('/')[-1])
 
